367759f06b6a61c44d59333c72e601760d10ce52
Tatyana Kulakova <117149308+tanyakul22@users.noreply.github.com>
2022-11-16 18:20:26 +1000

    DZ2

---
+++ Exs001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите пятизначное число для проверки на полиндром: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+int c = a;
+while(c > 0)
+{
+    b = b * 10 + c % 10;
+    c = c/10;
+}
+if (a==b) Console.Write("{0} число полиндром", a);
+else Console.Write("{0} число не полиндром", a);
---
+++ Exs002/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число для нахождения ряда кубов чисел до него начиная с 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[a];
+int i;
+for (i=0; i<a; i++)
+{
+    mass[i] = (i+1)*(i+1)*(i+1);
+    Console.WriteLine("{0} {1}", i+1, mass[i]);
+}
